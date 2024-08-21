7df9a49be0b45815f363c70d99c0891c359f55a2
ISASOTO99 <ISASOTO@uat.edu>
2024-08-20 17:31:09 -0700

    finished game

--- game.js
+++ game.js
@@ -3,5 +3,30 @@ function begingame(){
     var aichoice = Math.random();
     aichoice = aichoice*2
     aichoice = Math.round(aichoice);
-    alert(aichoice)
+
+    var userchoice = prompt("enter rock paper or scissors");
+    var usernum;
+    if(userchoice == "rock"){
+        usernum = 0
+    }else if(userchoice == "paper"){
+        usernum = 1
+    }else if(userchoice == "scissors"){
+        usernum = 2
+    }else{
+        return;
+    }
+
+    if(aichoice == usernum){
+        alert("IT WAS A TIE")
+    }
+    else if (usernum == 0 && aichoice == 1){
+        alert("YOU LOSE")
+    }else if(usernum == 1 && aichoice == 2){
+        alert("YOU LOSE")
+    }else if (usernum == 2 && aichoice == 0){
+        alert("YOU LOSE")
+    }else {
+        alert("YOU WON")
+    }
+
 }
